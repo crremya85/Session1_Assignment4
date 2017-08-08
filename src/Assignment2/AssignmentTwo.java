2ab1d7ed6e75fd68c5b5d7de8514796aa63c7ad9
Remya C R <crremya85@gmail.com>
2017-08-08 14:18:52 +0530

    Assignment2_Session1

---
+++ src/Assignment2/AssignmentTwo.java
@@ -0,0 +1,23 @@
+package Assignment2;
+
+/**
+ *  This class is created for addition of two numbers without using a '+' operator
+ *
+ */
+public class AssignmentTwo {
+
+	/**
+	 * Two variables are initialized and the sum is calculated using '-' operator and 
+	 * the result is stored in the third variable
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10, b=20,c;
+		c= a-(-b);
+		
+		// Using minus operator
+		System.out.println("Sum of a and b:"+c);
+	}
+
+}
